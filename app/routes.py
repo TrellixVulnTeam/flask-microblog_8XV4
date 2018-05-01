3d631a47791077fa956785a3afce37008d651773
Marcel <khouri10@gmail.com>
2018-05-01 17:02:31 -0300

    Chapter 4 (shell didn't work)

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,12 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'username': 'Marcel'}
-	posts = [
+    user = {'username': 'Miguel'}
+    posts = [
         {
             'author': {'username': 'John'},
             'body': 'Beautiful day in Portland!'
@@ -16,7 +17,8 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-	return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -25,4 +27,4 @@ def login():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)	
+    return render_template('login.html',  title='Sign In', form=form)
